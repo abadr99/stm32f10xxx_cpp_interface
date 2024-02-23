13bc89a35459d9485eeb23894950fd2a99e9a34d
abadr99 <badr@Ubuntu.myguest.virtualbox.org>
2024-02-23 16:33:22 +0200

    DBG

--- tools/github/configure.sh
+++ tools/github/configure.sh
@@ -3,5 +3,5 @@
 wget -P /opt https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a&hash=5631ACEF1F8F237389F14B41566964EC
 
 tar -xvjf /opt/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a
-
+ls
 export PATH="/opt/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a/gcc-arm-none-eabi-10.3-2021.10/bin:$PATH"
