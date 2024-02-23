9051045d2052b20e8caae4c839571fe160b1ae22
abadr99 <badr@Ubuntu.myguest.virtualbox.org>
2024-02-23 17:17:04 +0200

    DBG negative tests

--- dev/main.cpp
+++ dev/main.cpp
@@ -1,8 +1,15 @@
 /**
- * "Copyright [2024] <Copyright Owner>"
+ * @file main.cpp
+ * @author 
  * @brief 
+ * @version 0.1
+ * @date 2024-02-22
+ * 
+ * @copyright Copyright (c) 2024
  * 
  */
+
+#include "utils/inc/BitManipulation.h"
 
 int main() {
     while (1) { /* EMPTY */ }
--- dev/utils/inc/BitManipulation.h
+++ dev/utils/inc/BitManipulation.h
@@ -21,14 +21,14 @@ namespace bit_manipulation {
 
 template<typename T>
 inline constexpr T GetAllOnes() {
-    return static_cast<T>(-1)
+    return static_cast<T>(-1);
 }
 
 // Return specific number of ones
 // Example: GetOnes(4) --> 0b1111
 template<typename T>
 inline constexpr T GetOnes(const T NumberOfOnes) {
-    return (1 << NumberOfOnes) - 1;
+    return (1 << NumberOfOnes) - 1
 }
 
 template<typename T, uint8_t startBit, uint8_t endBit = startBit>
@@ -36,7 +36,7 @@ constexpr T ExtractBits(const T value) {
     static_assert(startBit <= endBit,
                   "Calling ExtractBits with startBit first");
     uint8_t numberOfBits = endBit - startBit + 1;
-    return (value >> startBit) & (utils::GetOnes<uint8_t>(numberOfBits));
+    return (value >> startBit) & (GetOnes<uint8_t>(numberOfBits));
 }
 
 }  // namespace bit_manipulation
