b2728d121d6943793f92152c36c66a70f1ef683f
abadr99 <badr@Ubuntu.myguest.virtualbox.org>
2024-02-23 16:35:12 +0200

    DBG

--- dbg.sh
+++ dbg.sh
@@ -1,6 +1,3 @@
 #ls /opt/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a
 ls /opt
-tar -xvjf /opt/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a
-ls /opt
 
-file /opt/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a
