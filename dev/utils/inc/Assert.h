212be9c7456b4ea7d2d7460dddd239aafa3a7560
abadr99 <badr@Ubuntu.myguest.virtualbox.org>
2024-03-11 23:59:29 +0200

    Add assert system

---
+++ dev/utils/inc/Assert.h
@@ -0,0 +1,28 @@
+/**
+ * @file Assert.h
+ * @brief 
+ * @version 0.1
+ * @date 2024-03-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef DEV_UTILS_INC_ASSERT_H_
+#define DEV_UTILS_INC_ASSERT_H_
+
+#include <cstddef>
+#include <assert.h>
+
+#define STM32_ASSERT(cond_, msg_)\
+    assert(cond_)
+
+#define ASSERT_STRUCT_SIZE(struct_type, expected_size) \
+    static_assert(sizeof(struct_type) == expected_size, \
+    "Struct size mismatch. The member order may have been modified.");
+
+#define ASSERT_MEMBER_OFFSET(struct_type, member_name, expected_offset) \
+    static_assert(offsetof(struct_type, member_name) == expected_offset, \
+        "Member offset mismatch. The member order may have been modified.");
+
+#endif  // DEV_UTILS_INC_ASSERT_H_
