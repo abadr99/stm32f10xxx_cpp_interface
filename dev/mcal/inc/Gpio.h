de9063d67ec494b75bbd3d04cdbfd94a93d4a703
noran97 <noranmahmoud597@gmail.com>
2024-03-16 04:52:43 +0200

    writing namespaces

---
+++ dev/mcal/inc/Gpio.h
@@ -0,0 +1,24 @@
+/**
+ * @file Gpio.h
+ * @author
+ * @brief
+ * @version 0.1
+ * @date 2024-03-16
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#ifndef DEV_MCAL_INC_GPIO_H_
+#define DEV_MCAL_INC_GPIO_H_
+
+namespace stm32 {
+namespace dev {
+namespace mcal {
+namespace gpio {
+
+
+}  // namespace gpio
+}  // namespace mcal
+}  // namespace dev
+}  // namespace stm32
+#endif  // DEV_MCAL_INC_GPIO_H_
