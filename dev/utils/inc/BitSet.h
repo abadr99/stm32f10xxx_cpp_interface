2d40f909565606c91e45a4ef6b08b8909a83198e
abadr99 <badr@Ubuntu.myguest.virtualbox.org>
2024-03-18 00:40:41 +0200

    Add Bitset data structure

---
+++ dev/utils/inc/BitSet.h
@@ -0,0 +1,50 @@
+/**
+ * @file BitSet.h
+ * @brief 
+ * @version 0.1
+ * @date 2024-03-17
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef DEV_BITSET_INC_MATH_H_
+#define DEV_BITSET_INC_MATH_H_
+
+#include "BitManipulation.h"
+
+namespace stm32 {
+namespace utils {
+namespace bitset {
+
+template <typename T>
+class BitSet {
+ public:
+
+    using utils = stm32::utils::bit_manipulation;
+    BitSet(const T bits) : bits_(bits)
+    { }
+
+    template <T FROM, T TO = FROM>
+    inline constexpr void SetValue(T val) {
+        utils::WriteBits<T. FROM, TO>(bits_);
+    }
+    
+    template <T FROM, T TO = FROM>
+    constexpr T GetValue() {
+        return utils::ExtractBits<FROM, TO>(bits_);
+    }
+
+    T operator[](T idx) {
+        return utils::ExtractBits<FROM, TO>(bits_);
+    }
+
+ private:
+    T bits_;
+};
+
+}   // namespace bitset
+}   // namespace utils
+}   // namespace stm32
+
+#endif // DEV_BITSET_INC_MATH_H_
---
+++ dev/utils/src/BitSet.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file BitSet.cpp
+ * @brief 
+ * @version 0.1
+ * @date 2024-03-17
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+// using namespace stm32::utils::bitset;
