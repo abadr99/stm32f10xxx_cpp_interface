ac0fe9a6ef9d47da5ce622e6bb16ca6d02cbeeb6
abadr99 <badr@Ubuntu.myguest.virtualbox.org>
2024-03-20 00:29:40 +0200

    Add header of pin

---
+++ dev/mcal/src/Pin.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file Pin.cpp
+ * @brief 
+ * @version 0.1
+ * @date 2024-03-19
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <stdint.h>
+#include "utils/inc/BitSet.h"
+#include "utils/inc/Assert.h"
+#include "mcal/inc/Pin.h"
+
