c105fd9c4fcc27eb9de186df1c5104f4d69f1966
MohamedRefat13 <mo7amedrefaat2001@gmail.com>
2024-03-26 02:14:24 +0200

    adding test file for Pin

--- tests/unittests/mcal/Pin-test.cpp
+++ tests/unittests/mcal/Pin-test.cpp
@@ -1,13 +1,13 @@
 /**
  * @file Pin-test.cpp
- * @author 
+ * @author
  * @brief 
  * @version 0.1
  * @date 2024-03-26
-
+ * 
+ * @copyright Copyright (c) 2024
  * 
  */
-
 #include <gtest/gtest.h>
 #include "utils/inc/BitManipulation.h"
 #include "mcal/inc/stm32f103xx.h"
