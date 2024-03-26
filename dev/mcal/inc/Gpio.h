7b9182191c342564042eacef095f09a1b5288dd2
noran97 <noranmahmoud597@gmail.com>
2024-03-26 15:05:22 +0200

    fixing function to suit new archeticture

--- dev/mcal/inc/Gpio.h
+++ dev/mcal/inc/Gpio.h
@@ -10,6 +10,7 @@
  */
 #ifndef DEV_MCAL_INC_GPIO_H_
 #define DEV_MCAL_INC_GPIO_H_
+using namespace stm32::dev::mcal::pin; // NOLINT[build/namespaces]
 using namespace stm32::registers::gpio; // NOLINT[build/namespaces]
 namespace stm32 {
 namespace dev {
@@ -21,69 +22,9 @@ enum State{
     kHigh,
 };
 
-enum Mode{
-    kIN_analog = 0,
-    kIN_floating = 4,
-    kIN_pullup = 8,
-    kIN_pulldown = 20,
-    kOP_pushpull_10MHZ = 1,
-    kOP_pushpull_2MHZ = 2,
-    kOP_pushpull_50MHZ = 3,
-    kOP_opendrain_10MHZ = 5,
-    kOP_opendrain_2MHZ = 6,
-    kOP_opendrain_50MHZ = 7,
-    kaf_pushpull_10MHZ = 9,
-    kaf_pushpull_2MHZ = 10,
-    kaf_pushpull_50MHZ = 11,
-    kaf_opendrain_10MHZ = 13,
-    kaf_opendrain_2MHZ = 14,
-    kaf_opendrain_50MHZ = 15,
-};
-
-enum Pin{
-    kpin0,
-    kpin1,
-    kpin2,
-    kpin3,
-    kpin4,
-    kpin5,
-    kpin6,
-    kpin7,
-    kpin8,
-    kpin9,
-    kpin10,
-    kpin11,
-    kpin12,
-    kpin13,
-    kpin14,
-    kpin15,
-};
 
 class Gpio{
  public:
- /**
- * @brief Set the Mode of a GPIO pin
- * 
- * @param GPIOx     [GPIOA, GPIOB, GPIOC]
- * @param pinNum    [kpin0 --> kpin15]
- * @param mode      options : 1- kIN_analog
- *                            2- kIN_floating
- *                            3- kIN_pullup
- *                            4- kIN_pulldown
- *                            5- kOP_pushpull_10MHZ
- *                            6- kOP_pushpull_2MHZ
- *                            7- kOP_pushpull_50MHZ
- *                            8- kOP_opendrain_10MHZ
- *                            9- kOP_opendrain_2MHZ
- *                            10- kOP_opendrain_50MHZ
- *                            11- kaf_pushpull_10MHZ
- *                            12- kaf_pushpull_2MHZ
- *                            13- kaf_pushpull_50MHZ
- *                            14- kaf_opendrain_10MHZ
- *                            15- kaf_opendrain_2MHZ
- *                            16- kaf_opendrain_50MHZ
- */
-    static void SetPinMode(volatile GpioRegDef* GPIOx, Pin pinNum, Mode mode); // NOLINT
 /**
  * @brief Set the value of a GPIO pin
  * 
@@ -91,7 +32,7 @@ class Gpio{
  * @param pinNum    [kpin0 --> kpin15]
  * @param pinState  [klow, khigh]
  */
-    static void SetPinValue(volatile GpioRegDef* GPIOx, Pin pinNum, State pinState);  // NOLINT
+void SetPinValue(Pin pin, State pinState);
  /**
  * @brief Get the value of a GPIO pin
  * 
@@ -99,7 +40,7 @@ class Gpio{
  * @param pinNum    [kpin0 --> kpin15]
  * @param ReturnVal Pointer to store the returned value
  */
-    static void GetPinValue(volatile GpioRegDef* GPIOx, Pin pinNum, uint32_t * pPinValue);    // NOLINT
+   // static void GetPinValue(volatile GpioRegDef* GPIOx, Pin pinNum, uint32_t * pPinValue);    // NOLINT
 };
 }  // namespace gpio
 }  // namespace mcal
--- dev/mcal/src/Gpio.cpp
+++ dev/mcal/src/Gpio.cpp
@@ -10,27 +10,13 @@
  */
 #include "mcal/inc/stm32f103xx.h"
 #include "utils/inc/Assert.h"
+#include "mcal/inc/Pin.h"
 #include "mcal/inc/Gpio.h"
 
-using namespace stm32::dev::mcal::gpio; // NOLINT[build/namespaces]
+using namespace stm32::dev::mcal::pin; // NOLINT[build/namespaces]
 using namespace stm32::registers::gpio; // NOLINT[build/namespaces]
+using namespace stm32::dev::mcal::gpio; // NOLINT[build/namespaces]
 
-#define ISMODE_VALID(MODE) ((MODE == kIN_analog)          || \
-                            (MODE == kIN_floating)        || \
-                            (MODE == kIN_pullup)          || \
-                            (MODE == kIN_pulldown)        || \
-                            (MODE == kOP_pushpull_10MHZ)  || \
-                            (MODE == kOP_pushpull_2MHZ)   || \
-                            (MODE == kOP_pushpull_50MHZ)  || \
-                            (MODE == kOP_opendrain_10MHZ) || \
-                            (MODE == kOP_opendrain_2MHZ)  || \
-                            (MODE == kOP_opendrain_50MHZ) || \
-                            (MODE == kaf_pushpull_10MHZ)  || \
-                            (MODE == kaf_pushpull_2MHZ)   || \
-                            (MODE == kaf_pushpull_50MHZ)  || \
-                            (MODE == kaf_opendrain_10MHZ) || \
-                            (MODE == kaf_opendrain_2MHZ)  || \
-                            (MODE == kaf_opendrain_50MHZ))
 
 ASSERT_STRUCT_SIZE(GpioRegDef, (sizeof(RegWidth_t) * 7));
 
@@ -42,29 +28,22 @@ ASSERT_MEMBER_OFFSET(GpioRegDef, BSRR, sizeof(RegWidth_t) * 4);
 ASSERT_MEMBER_OFFSET(GpioRegDef, BRR, sizeof(RegWidth_t) * 5);
 ASSERT_MEMBER_OFFSET(GpioRegDef, LCKR, sizeof(RegWidth_t) * 6);
 
-void Gpio::SetPinMode(volatile GpioRegDef* GPIOx, Pin pinNum, Mode mode) {
-    STM32_ASSERT(ISMODE_VALID(mode));
-    STM32_ASSERT((pinNum >= 0 && pinNum <= 7) || (pinNum >= 8 && pinNum <= 15));    // NOLINT
+volatile GpioRegDef *GPIO[3] = {GPIOA, GPIOB, GPIOC};
 
-    if ((pinNum >= 0 && pinNum <= 7)) {
-        GPIOx->CRL &= (~(0b1111 << (pinNum*4)));
-        GPIOx->CRL |= (mode << (pinNum*4));
-    } else {
-        GPIOx->CRH &= (~(0b1111 << (pinNum*4)));
-        GPIOx->CRH |= (mode << (pinNum*4));
-    }
-}
-void Gpio::SetPinValue(volatile GpioRegDef* GPIOx, Pin pinNum, State pinState) {
-    STM32_ASSERT((pinNum >= 0 && pinNum <= 7) || (pinNum >= 8 && pinNum <= 15));    // NOLINT
+void Gpio::SetPinValue(Pin pin, State pinState) {
     if (pinState == kLow) {
-        GPIOx -> ODR &= ~(1 << pinNum);
-        //  GPIOx -> BRR |= (1 << pinNum);
+        GPIO[pin.GetPort()]->ODR &= ~(1 << pin.GetPinNumber());
+        // GPIOx -> ODR &= ~(1 << pinNum);
+        GPIO[pin.GetPort()]->BRR |= (1 << pin.GetPinNumber());
+        // GPIOx -> BRR |= (1 << pinNum);
     } else if (pinState == kHigh) {
-        GPIOx -> ODR |= (1 << pinNum);
+        GPIO[pin.GetPort()]->ODR |= (1 << pin.GetPinNumber());
+       // GPIOx -> ODR |= (1 << pinNum);
+       GPIO[pin.GetPort()]->BSRR |= (1 << pin.GetPinNumber());
         //  GPIOx -> BSRR |= (1 << pinNum);
     }
 }
-void Gpio::GetPinValue(volatile GpioRegDef* GPIOx, Pin pinNum, uint32_t * pPinValue) {    // NOLINT
+/*void Gpio::GetPinValue(volatile GpioRegDef* GPIOx, Pin pinNum, uint32_t * pPinValue) {    // NOLINT
     STM32_ASSERT((pinNum >= 0 && pinNum <= 7) || (pinNum >= 8 && pinNum <= 15));    // NOLINT
     *pPinValue = GPIOx ->IDR &(1 << pinNum) >> pinNum;
-}
+}*/
