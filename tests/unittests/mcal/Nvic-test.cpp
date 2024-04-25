081a3c7167d412b57dff85125c611a356c11d5ff
noura36 <nouramohamedgamalll@gmail.com>
2024-04-25 17:01:01 +0200

    fix error

---
+++ tests/unittests/mcal/Nvic-test.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file Nvic-test.cpp
+
+ * @brief
+ * @version 0.1
+ * @date 2024-04-25
+ * @copyright Copyright (c) 2024
+ *
+ */
+#include <gtest/gtest.h>
+#include "utils/inc/BitManipulation.h"
+#include "mcal/inc/stm32f103xx.h"
+#include "mcal/inc/Nvic.h"
+
+
+uint32_t NvicReg[10] = {};
+uint32_t ScbReg[10] = {};
+
+using namespace stm32::utils::bit_manipulation;
+using namespace stm32::dev::mcal::nvic;
+using namespace stm32::registers::nvic;
