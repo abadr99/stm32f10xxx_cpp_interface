01eb5b3610b05842d75e1f7edcef78ba1aaf19f1
noran97 <noranmahmoud597@gmail.com>
2024-04-29 15:55:17 +0300

    adding keypad functions

--- dev/hal/inc/Keypad.h
+++ dev/hal/inc/Keypad.h
@@ -15,7 +15,11 @@ using namespace stm32::dev::mcal::gpio;
 template<uint8_t Rows, uint8_t Columns>
 class Keypad {
  public:
+      void SetRowArr(Pin rows[Rows]);
+      void setColArr(Pin cols[Columns]);
       void KeypadInit();
+      uint8_t GetPressed(uint8_t keypadButtons[Rows][Columns]);
+ private:
       Pin KeypadRow[Rows];
       Pin KeypadCol[Columns];
 };
--- dev/hal/src/Keypad.cpp
+++ dev/hal/src/Keypad.cpp
@@ -15,12 +15,37 @@
 #include "hal/inc/Keypad.h"
 using  namespace stm32::dev::mcal::pin;
 using  namespace stm32::dev::mcal::gpio;
+
+template<uint8_t Rows, uint8_t Columns>
+void Keypad<Rows, Columns>::SetRowArr(Pin rows[Rows]) {
+    for (uint8_t numRow =0 ; numRow <Rows; numRow++) {
+        KeypadRow[numRow] = rows[numRow];
+    }
+}
+template<uint8_t Rows, uint8_t Columns>
+void Keypad<Rows, Columns>::setColArr(Pin cols[Columns]) {
+    for (uint8_t numCol =0; numCol <Columns; numCol++) {
+        KeypadCol[numCol] = cols[numCol];
+    }
+}
 template<uint8_t Rows, uint8_t Columns>
 void Keypad<Rows, Columns>::KeypadInit() {
     for (uint8_t numRow =0 ; numRow <Rows; numRow++) {
-        SetInputMode(KeypadRow[numRow], stm32::dev::mcal::gpio::InputMode::kPullup);
+        SetInputMode(KeypadRow[numRow], InputMode::kPullup);
     }
     for (uint8_t numCol =0; numCol <Columns; numCol++) {
-        SetOutputMode(KeypadCol[numCol], stm32::dev::mcal::gpio::OutputMode::kPushPull_2MHZ);
+        SetOutputMode(KeypadCol[numCol], OutputMode::kPushPull_2MHZ);
+    }
+}
+template<uint8_t Rows, uint8_t Columns>
+uint8_t Keypad<Rows, Columns>::GetPressed(uint8_t keypadButtons[Rows][Columns]) {
+    for (uint8_t numCol =0; numCol <Columns; numCol++) {
+        SetPinValue(KeypadCol[numCol], kLow);
+        for (uint8_t numRow =0 ; numRow <Rows; numRow++) {
+            if (GetPinValue(KeypadRow[numRow]) == kLow) {
+                return keypadButtons[numRow][numCol];
+            }
+        }
+        SetPinValue(KeypadCol[numCol], kHigh);
     }
 }
