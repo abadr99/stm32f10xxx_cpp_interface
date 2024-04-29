4043a303bb5913b218b3a2eb23af67162a8f8561
MohamedRefat <mo7amedrefaat2001@gmail.com>
2024-04-29 18:20:08 +0300

    Solving upload bug

--- dev/main.cpp
+++ dev/main.cpp
@@ -10,31 +10,7 @@
  */
 
 #include "utils/inc/BitManipulation.h"
-#include "mcal/inc/stm32f103xx.h"
-#include "mcal/inc/Rcc.h"
-#include "mcal/inc/Pin.h"
-#include "mcal/inc/Gpio.h"
-using namespace stm32::utils::bit_manipulation;
-using namespace stm32::dev::mcal::rcc;
-using namespace stm32::dev::mcal::pin;
-using namespace stm32::dev::mcal::gpio;
+
 int main() {
-    Rcc::SetExternalClock(kHseCrystal);
-    Rcc::InitSysClock();
-    Pin pc13(kPortA,kPin13,PinMode::kOutput);
-    Gpio::SetOutputMode(pc13, OutputMode::kPushPull_10MHZ);
-    Gpio::SetPinValue(pc13, kLow);
-    // uint16_t Local_u16Counter;
-    while (1) {
-        // Gpio::SetPinValue(pc13, kLow);
-    	// for(Local_u16Counter = 0 ; Local_u16Counter < 50000 ; Local_u16Counter++)
-		// {
-		// 	__asm("NOP");
-		// }
-        // Gpio::SetPinValue(pc13, kLow);
-    	// 	for(Local_u16Counter = 0 ; Local_u16Counter < 50000 ; Local_u16Counter++)
-		// {
-		// 	__asm("NOP");
-		// }
-    }
+    while (1) { /* EMPTY */ }
 }
