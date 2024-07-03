b1b31f7277f15b7dc8a060f08f7b2f28937ed3b6
nouragamal <noyragamal@noura.myguest.virtualbox.org>
2024-07-04 01:54:25 +0300

    add EXTI test

--- tests/unittests/mcal/EXTI-test.cpp
+++ tests/unittests/mcal/EXTI-test.cpp
@@ -1,9 +1,9 @@
 /**
  * @file EXTI-test.cpp
- * @author Manar
+ * @author Noura
  * @brief 
  * @version 0.1
- * @date 2024-04-29
+ * @date 2024-07-03
  * 
  * @copyright Copyright (c) 2024
  * 
@@ -12,9 +12,50 @@
 #include "utils/inc/BitManipulation.h"
 #include "mcal/inc/stm32f103xx.h"
 #include "mcal/inc/Pin.h"
+#include "mcal/inc/EXTI.h"
 
 using namespace stm32::utils::bit_manipulation;
+using namespace stm32::dev::mcal::pin;
+using namespace stm32::dev::mcal::exti;
 using namespace stm32::registers::exti;
+using namespace stm32::registers::afio;
+using namespace stm32::registers::gpio; 
 
 uint32_t AFIOReg[7] = {0x0};
 uint32_t EXTIReg[6] = {0x0};
+
+TEST(EXTItest, Enable) {
+    EXTI_Config exti;
+    exti.port = kPortA;
+    exti.line = Line::EXTI0;
+    exti.trigger = Trigger::Rising;
+    Exti::Enable(exti);
+    EXPECT_EQ(0x0,    AFIO->EXTICRx[0]);
+    EXPECT_EQ(0x1,       EXTI->IMR);
+    EXPECT_EQ(0x1,       EXTI->RTSR);
+}
+TEST(EXTItest, Disable) {
+    EXTI_Config exti;
+    exti.port = kPortB;
+    exti.line = Line::EXTI10;
+    exti.trigger = Trigger::Falling;
+    Exti::Disable(exti);
+    EXPECT_EQ(0x1,      EXTI->IMR);  //  EXTI0 is enable in previous test
+    EXPECT_EQ(0x0,      EXTI->FTSR);
+}
+TEST(EXTItest, SetPendingFlag) {
+    EXTI_Config exti;
+    exti.port = kPortB;
+    exti.line = Line::EXTI6;
+    exti.trigger = Trigger::Falling;
+    Exti::SetPendingFlag(exti);
+    EXPECT_EQ(0x40,       EXTI->PR);
+}
+TEST(EXTItest, ClearPendingFlag) {
+    EXTI_Config exti;
+    exti.port = kPortB;
+    exti.line = Line::EXTI16;
+    exti.trigger = Trigger::Falling;
+    Exti::ClearPendingFlag(exti);
+    EXPECT_EQ(0x40,       EXTI->PR);
+}
