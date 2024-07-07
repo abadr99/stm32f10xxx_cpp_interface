6fb009f0c2f3984edaf24fe4adb0758cb93ecb28
gehadHassanein <gehadelmenshawy2@gmail.com>
2024-07-07 10:36:39 +0300

    adding buzzer functios

---
+++ dev/hal/src/Buzzer.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file Buzzer.cpp
+ * @author
+ * @brief
+ * @version 0.1
+ * @date 2024-07-7
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#include "mcal/inc/Pin.h"
+#include "mcal/inc/Gpio.h"
+#include "hal/inc/Buzzer.h"
+
+using namespace stm32::dev::mcal::pin;
+using namespace stm32::dev::mcal::gpio;
+using namespace stm32::dev::hal::buzzer;
+
+Buzzer::Buzzer(const Pin buzzerPin, const ConnectionType connectionType)
+    : buzzerPin_(buzzerPin), connectionType_(connectionType) {
+        Gpio::SetOutputMode(buzzerPin_, OutputMode::kPushPull_2MHZ);
+}
+
+void Buzzer::TurnOn() {
+    Gpio::SetPinValue(buzzerPin_, static_cast<State>(!connectionType_));
+    buzzerState_ = BuzzerState::kOn;
+}
+
+void Buzzer::TurnOff() {
+    Gpio::SetPinValue(buzzerPin_, static_cast<State>(connectionType_));
+    buzzerState_ = BuzzerState::kOff;
+}
+
+void Buzzer::Toggle() {
+    (buzzerState_ == BuzzerState::kOn) ? TurnOff() : TurnOn();
+}
