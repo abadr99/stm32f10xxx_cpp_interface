7d8af09729a9216f1d5690ddd5cbffb4fb2bf9bb
Mohamed-Refat <mo7amedrefaat2001@gmail.com>
2024-07-08 11:25:33 +0300

    Fixing style

--- tests/hardware-app/mcal/gpio/main.cpp
+++ tests/hardware-app/mcal/gpio/main.cpp
@@ -1,9 +1,19 @@
-#include "stm32f103xx.h"
-#include "BitManipulation.h"
-#include "Pin.h"
-#include "Gpio.h"
-#include "Rcc.h"
-#include "Systick.h"
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-07-08
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include "utils/inc/stm32f103xx.h"
+#include "utils/inc/BitManipulation.h"
+#include "mcal/inc/Pin.h"
+#include "mcal/inc/Gpio.h"
+#include "mcal/inc/Rcc.h"
+#include "mcal/inc/Systick.h"
 
 using namespace stm32::registers::rcc;
 using namespace stm32::dev::mcal::pin;
@@ -20,11 +30,11 @@ int main() {
     uint32_t Local_u16Counter = 0;
     while (1) {
         Gpio::SetPinValue(pc13, kLow);
-        for(Local_u16Counter = 0 ; Local_u16Counter < 50000 ; Local_u16Counter++) {
+        for (Local_u16Counter = 0; Local_u16Counter < 50000; Local_u16Counter++) {
             __asm("NOP");
         }
         Gpio::SetPinValue(pc13, kLow);
-        for(Local_u16Counter = 0 ; Local_u16Counter < 50000 ; Local_u16Counter++) {
+        for (Local_u16Counter = 0; Local_u16Counter < 50000; Local_u16Counter++) {
              __asm("NOP");
         }
     }
