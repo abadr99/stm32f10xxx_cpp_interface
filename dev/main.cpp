ab7c4c82af6e8cf94df75054f0be39c54f7d52fc
Mohamed Refat <93578161+MRefat13@users.noreply.github.com>
2024-07-18 01:57:03 +0300

    Adding required confi@Mrefat13, @abadr99] (#34)

--- dev/main.cpp
+++ dev/main.cpp
@@ -1,16 +1,15 @@
 /**
  * @file main.cpp
- * @author 
+ * @author your name (you@domain.com)
  * @brief 
  * @version 0.1
- * @date 2024-02-22
+ * @date 2024-07-08
  * 
  * @copyright Copyright (c) 2024
  * 
  */
-
-#include "utils/inc/BitManipulation.h"
+#include "mcal/inc/stm32f103xx.h"
 
 int main() {
-    while (1) { /* EMPTY */ }
+    while (1) {}
 }
