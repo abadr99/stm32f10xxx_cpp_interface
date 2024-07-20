d336403f6d9a35b8f8bb1dc1318be008c4526226
Noura Gamal <116299689+noura36@users.noreply.github.com>
2024-07-20 15:57:10 +0300

    Add SPI[@noura36,@noran97] (#25)
    Co-authored-by: noran97 <noranmahmoud597@gmail.com>
    Co-authored-by: nouragamal <noyragamal@noura.myguest.virtualbox.org>
    Co-authored-by: noran97 <99215076+noran97@users.noreply.github.com>
    Co-authored-by: Abdelrahman Badr <abdelrhmanezzbadr@outlook.com>

--- tests/unittests/mcal/BaseAddress-test.h
+++ tests/unittests/mcal/BaseAddress-test.h
@@ -16,9 +16,12 @@
 
 #define RCC_BASE_ADDRESS (&RccReg[0])
 
-#define GPIOA_BASE_ADDRESS  (&GPIOAReg[0])
-#define GPIOB_BASE_ADDRESS  (&GPIOBReg[0])
-#define GPIOC_BASE_ADDRESS  (&GPIOCReg[0])
+#define GPIOA_BASE_ADDRESS      (&GPIOAReg[0])
+#define GPIOB_BASE_ADDRESS      (&GPIOBReg[0])
+#define GPIOC_BASE_ADDRESS      (&GPIOCReg[0])
+
+#define SPI1_BASE_ADDRESS        (&SpiReg[0])
+#define SPI2_BASE_ADDRESS        (&SpiReg[1])
 
 #define NVIC_BASE_ADDRESS   (&NvicReg[0])
 #define SCB_BASE_ADDRESS    (&ScbReg[0])
@@ -36,7 +39,9 @@ extern uint32_t RccReg[10];
 extern uint32_t GPIOAReg[7];
 extern uint32_t GPIOBReg[7];
 extern uint32_t GPIOCReg[7];
+
 extern uint32_t SYSTICKReg[3];
+extern uint32_t SpiReg[8];
 
 extern uint32_t USART1Reg[7];
 extern uint32_t USART2Reg[7];
