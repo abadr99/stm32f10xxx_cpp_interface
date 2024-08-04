0cdfff009238ebfc518b493f2622811dc476128a
gehad hassanein <149859144+gehadHassanin@users.noreply.github.com>
2024-08-04 22:56:36 +0300

    Add buzzer driver (#29)

---
+++ dev/hal/inc/Buzzer.h
@@ -0,0 +1,45 @@
+/**
+ * @file Buzzer.h
+ * @author
+ * @brief
+ * @version 0.1
+ * @date 2024-07-7
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#ifndef DEV_HAL_INC_BUZZER_H_
+#define DEV_HAL_INC_BUZZER_H_
+#include "mcal/inc/Pin.h"
+using namespace stm32::dev::mcal::pin;
+namespace stm32 {
+namespace dev {
+namespace hal {
+namespace buzzer {
+
+enum ConnectionType : uint8_t {
+    kForward_Bias,  // active high
+    kReverse_Bias,  // active low
+};
+
+enum BuzzerState : uint8_t {
+    kOn, kOff
+};
+
+class Buzzer {
+ public:
+    Buzzer(const Pin buzzerPin, const ConnectionType connectionType);
+    void TurnOn();
+    void TurnOff();
+    void Toggle();
+    BuzzerState GetBuzzerState();
+ private:
+    BuzzerState buzzerState_;
+    Pin buzzerPin_;
+    ConnectionType connectionType_;
+};
+}  // namespace buzzer
+}  // namespace hal
+}  // namespace dev
+}  // namespace stm32
+#endif  // DEV_HAL_INC_BUZZER_H_
