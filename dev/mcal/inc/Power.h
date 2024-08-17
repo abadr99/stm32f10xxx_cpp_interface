41e3e74caf1604c2d12fbc642981cff28fea0a73
Noura Gamal <116299689+noura36@users.noreply.github.com>
2024-08-17 19:15:09 +0300

    Support Power Saving [@noura36, @MRefat13] (#62)
    Co-authored-by: nouragamal <noyragamal@noura.myguest.virtualbox.org>
    Co-authored-by: Mohamed-Refat <mo7amedrefaat2001@gmail.com>
    Co-authored-by: Mohamed Refat <93578161+MRefat13@users.noreply.github.com>

---
+++ dev/mcal/inc/Power.h
@@ -0,0 +1,60 @@
+/**
+ * @file Power.h
+ * @author noura36
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-13
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#ifndef DEV_MCAL_INC_POWER_H_
+#define DEV_MCAL_INC_POWER_H_
+namespace stm32 {
+namespace dev   {
+namespace mcal  {
+namespace pwr {
+
+enum class SleepType {
+    kSleepNow,
+    kSleepOnExit
+};
+enum class PwrRegulator {
+    kOn,
+    kLowPower
+};
+
+enum class PwrEntry {
+    kWFI,  //  wait for interrupt
+    kWFE   //  wait for event
+};
+enum class State {
+    kDisable,
+    kEnable
+};
+enum class PwrFlag {
+    kWU,  //  wakeup flag
+    kSB   //  standby flag
+};
+
+class Pwr {
+ public:
+    static void EnterSleepMode(PwrEntry sleepEntry, SleepType type);
+    static void EnterStopMode(PwrEntry stopEntry, PwrRegulator regulator);
+    static void EnterStandbyMode(PwrEntry standbyEntry);
+/**
+ * @note this is used for wakeup from Standby mode and forced in input pull down
+            configuration (rising edge on pin wakes-up the system from Standby mode)
+ */
+    static void WakeupPinState(State state);
+    static void ClearFlag(PwrFlag flag);
+ private:
+    #ifndef UNIT_TEST
+        static void EnterLowPowerMode(PwrEntry entry);
+    #endif
+};
+}   // namespace pwr
+}   // namespace mcal
+}   // namespace dev
+}   // namespace stm32
+#endif  //  DEV_MCAL_INC_POWER_H_
--- tests/unittests/mcal/BaseAddress-test.h
+++ tests/unittests/mcal/BaseAddress-test.h
@@ -43,6 +43,9 @@
 #define RTC_BASE_ADDRESS    (&RtcReg[0])
 #define WWDG_BASE_ADDRESS   (&WwdgReg[0])
 
+
+#define PWR_BASE_ADDRESS    (&PwrReg[0])
+
 extern uint32_t RccReg[10];
 
 extern uint32_t GPIOAReg[7];
@@ -74,6 +77,7 @@ extern uint32_t WwdgReg[3];
 extern uint32_t DmaReg[30];
 extern uint32_t RtcReg[10];
 
+extern uint32_t PwrReg[2];
 extern uint32_t IwdgReg[4];
 
 #endif  // TESTS_UNITTESTS_MCAL_BASEADDRESS_TEST_H_
