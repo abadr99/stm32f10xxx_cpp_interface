ac3ab169459d40eafcbdcf954f82388b270d1097
Mohamed Refat <93578161+MRefat13@users.noreply.github.com>
2024-08-24 19:03:12 +0300

    Fix memory issue with strings [@MRefat13, @abadr99] (#66)
    Co-authored-by: Abdelrahman Badr <abdelrhmanezzbadr@outlook.com>

--- dev/hal/src/Esp.cpp
+++ dev/hal/src/Esp.cpp
@@ -9,29 +9,29 @@
  * 
  */
 
-#include <string>
+// #include <cstring>
+// #include <string>
 #include "Assert.h"
 #include "Usart.h"
 #include "Esp.h"
 
 #define ESP_BAUD_RATE  (115200)
 
-#define AT_CWMODE     "AT+CWMODE"
-#define AT_RST        "AT+RST"
-#define AT_CWJAP_CUR  "AT+CWJAP_CUR"
-#define ATE           "ATE"  
-
-#define AT_SET_POSTFIX  "="  
-#define AT_GET_POSTFIX  "?"
-#define AT_END          "\r\n"
-#define COMMA           std::string(",")
+static constexpr const char* commandStrings[] = {
+    "AT\r\n",    
+    "AT+RST\r\n",   
+    "ATE",
+    "AT+CWMODE",
+    "AT+CWJAP_CUR",
+    "AT+CIPSTART",
+    "=",
+    "?",
+    "\r\n",
+    ","
+};
 
 #define TCP "TCP"
 #define UDP "UDP"
-
-
-#define AT_SET(str)     std::string(str) + std::string(AT_SET_POSTFIX)
-#define AT_GET(str)     std::string(str) + std::string(AT_GET_POSTFIX)
 
 using namespace stm32::dev::mcal::usart;
 using namespace stm32::dev::hal::esp;
@@ -50,52 +50,83 @@ Esp::Esp(const UsartNum &usartNum)
     }
 
 
-void Esp::Send(const std::string str) {
-    for (auto c : str) {
-        Send(c);
+void Esp::Send(const char* str) {
+    uint32_t str_idx = 0;
+    while (str[str_idx] != '\0') {
+        usart_.Transmit(str[str_idx++]);
     }
 }
 
-void Esp::Send(const char c) {
-    usart_.Transmit(c);
+void Esp::Send(Usart::DataValType n) {
+    usart_.Transmit(n);
 }
 
-void Esp::SetMode(const EspMode &mode) {
-    std::string cmd = std::string(AT_SET(AT_CWMODE))
-                     + std::to_string(static_cast<uint32_t>(mode))
-                     + std::string(AT_END);
-    Send(cmd);
+void Esp::SetMode(const EspMode &mode) {    
+    this->Send(commandStrings[Commands::AT_CWMODE]);
+    this->Send(commandStrings[Commands::kSET_POSTFIX]);
+    char cMode = static_cast<uint8_t>(mode) + 48;
+    this->Send(cMode);
+    this->Send(commandStrings[Commands::kAT_END]);
 }
 
 void Esp::SetEchoState(const EchoState &state) {
-    std::string cmd = std::string(ATE)
-                     + std::to_string(static_cast<uint32_t>(state))
-                     + std::string(AT_END);
-    Send(cmd);
+    this->Send(commandStrings[Commands::ATE]);
+    char cState = static_cast<uint8_t>(state) + 48;
+    this->Send(cState);
+    this->Send(commandStrings[Commands::kAT_END]);
 }
 
-void Esp::WiFiConnect(const std::string username, const std::string pswd) {
-    std::string cmd = std::string(AT_SET(AT_CWJAP_CUR)) 
-                    + username + pswd
-                    + std::string(AT_END);
-    Send(cmd);              
+void Esp::WiFiConnect(const char* username, const char* pswd) {
+    this->Send(commandStrings[Commands::AT_CWJAP_CUR]);
+    this->Send(commandStrings[Commands::kSET_POSTFIX]);
+    this->Send(username);
+    this->Send(commandStrings[Commands::kCOMMA]);
+    this->Send(pswd);
+    this->Send(commandStrings[Commands::kAT_END]);
 }
 
-void Esp::ServerConnect(const std::string protocol, const std::string ip, uint16_t port) {
-    std::string cmd = std::string(AT_SET(AT_CWJAP_CUR)) 
-                    + protocol + COMMA
-                    + ip + COMMA
-                    + std::to_string(port)
-                    + std::string(AT_END);
-    Send(cmd); 
+void Esp::ServerConnect(const char* protocol, const char* ip, uint16_t port) {
+    this->Send(commandStrings[Commands::AT_CIPSTART]);
+    this->Send(commandStrings[Commands::kSET_POSTFIX]);
+    this->Send(protocol);
+    this->Send(commandStrings[Commands::kCOMMA]);
+    this->Send(ip);
+    this->Send(commandStrings[Commands::kCOMMA]);
+    char strPort[6] = {0};
+    Helper_IntToString(port, strPort);
+    Send(strPort);
+    this->Send(commandStrings[Commands::kAT_END]);
 }
 
-void Esp::SendHttpRequest(const std::string request) {
-    std::string cmd = request + std::string(AT_END);
-    Send(cmd);
+
+void Esp::Helper_IntToString(int num, char *str) {
+    int index = 0;
+    int  negFlag = 0;
+    // Handle the case when the number is 0
+    if (num == 0) {
+        str[index++] = '0';
+        str[index] = '\0';
+        return;
+    }   
+    // Handle the case when the number is negative
+    if (num < 0) {
+        num *= -1;
+        str[index++] = '-';
+        negFlag = 1;
+    }
+
+    // Process each digit of the number
+    while (num != 0) {
+        str[index] = (num % 10) + '0';  //  Convert digit to character
+        num /= 10;  //   Remove the processed digit
+        index++;
+    }
+    str[index] = '\0';  //  Null-terminate the string
+
+    //  Reverse the string to get the correct order
+    for (int i = negFlag; i < index / 2; i++) {
+        char temp = str[i];
+        str[i] = str[index - i - 1];
+        str[index - i - 1] = temp;
+    }
 }
-
-
-// void Esp::RecvHttpResponse(const std::string response) {
-    
-// }
