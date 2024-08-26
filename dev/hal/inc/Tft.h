3c8a6619641241af7985f06dd5790b6c9e2461ac
nouragamal <nouragamal@ubuntu.myguest.virtualbox.org>
2024-08-26 20:24:52 +0300

    changes

--- dev/hal/inc/Tft.h
+++ dev/hal/inc/Tft.h
@@ -37,22 +37,35 @@ enum TftColors {
     kYellow  = 0xFFE0,
     kWhite   = 0xFFFF
 };
+enum TftResolution {
+    k128x160,   //   Common resolution for small TFT displays
+    k240x320,   //   Another common resolution
+    k320x480,   //   Larger resolution
+    k480x800    //   High-resolution TFT display
+};
+struct TftConfig {
+    Pin A0;
+    Pin rst;
+    Spi TFtSpi;
+    TftResolution resolution;  // TFT display resolution
+    uint16_t width;
+    uint16_t height;
+};
 class Tft {
  public:
-    Tft(Pin A0, Pin rst, Spi SdSpi);
+    explicit Tft(const TftConfig& config);
     void Reset();
     void DisplayImage(const Array<uint16_t, 20480> image);
     void DrawRectangle(uint8_t hight, uint8_t width, uint8_t xaxis, uint8_t yaxis, TftColors color);
  private:
-    const Pin A0;
-    const Pin rst;
-    Spi TFtSpi;
+    TftConfig config_;
     void SendCommand(uint8_t command);
     void SendData(uint8_t data);
     void SetAddress(uint16_t startX, uint16_t endX, uint16_t startY, uint16_t endY);
     void SetPixel(uint16_t pixel);
-    void DrawHLine(uint8_t xaxis, uint8_t yaxis, uint8_t length, TftColors color);
-    void DrawVLine(uint8_t xaxis, uint8_t yaxis, uint8_t length, TftColors color);
+    void DrawHLine(uint8_t xAxis, uint8_t yAxis, uint8_t length, TftColors color);
+    void DrawVLine(uint8_t xAxis, uint8_t yAxis, uint8_t length, TftColors color);
+    void InitializeResolution();
 };
 }   //  namespace tft
 }   //  namespace hal
--- dev/hal/src/Tft.cpp
+++ dev/hal/src/Tft.cpp
@@ -3,12 +3,13 @@
  * @author noura36
  * @brief
  * @version 0.1
- * @date 2024-08-23
+ * @date 2024-08-26
  *
  * @copyright Copyright (c) 2024
  *
  */
 #include "mcal/inc/stm32f103xx.h"
+#include "BitManipulation.h"
 #include "Pin.h"
 #include "Gpio.h"
 #include "Spi.h"
@@ -21,11 +22,13 @@ using namespace stm32::dev::mcal::pin;
 using namespace stm32::dev::mcal::spi;
 using namespace stm32::dev::mcal::systick;
 using namespace stm32::utils::array;
+using namespace stm32::utils::bit_manipulation;
 using namespace stm32::dev::hal::tft;
 
-Tft::Tft(Pin A0, Pin rst, Spi TFtSpi) : A0(A0), rst(rst), TFtSpi(TFtSpi) {
-    Gpio::Set(A0);
-    Gpio::Set(rst);
+Tft::Tft(const TftConfig& config) : config_(config) {
+    InitializeResolution();
+    Gpio::Set(config_.A0);
+    Gpio::Set(config_.rst);
     //  Reset pulse
     Reset();
     //  SleepOut
@@ -33,20 +36,44 @@ Tft::Tft(Pin A0, Pin rst, Spi TFtSpi) : A0(A0), rst(rst), TFtSpi(TFtSpi) {
     Systick::Delay_ms(150);
     //  Color mode
     SendCommand(kCOLMOD);
-    SendCommand(RGB565);
+    SendData(RGB565);
     //  Display on
     SendCommand(kDISPON);
 }
+void Tft::InitializeResolution() {
+    switch (config_.resolution) {
+        case k128x160:
+            config_.width = 128;
+            config_.height = 160;
+            break;
+        case k240x320:
+            config_.width = 240;
+            config_.height = 320;
+            break;
+        case k320x480:
+            config_.width = 320;
+            config_.height = 480;
+            break;
+        case k480x800:
+            config_.width = 480;
+            config_.height = 800;
+            break;
+        default:
+            config_.width = 128;  // Default to 128x160
+            config_.height = 160;
+            break;
+    }
+}
 void Tft::Reset() {
-    Gpio::SetPinValue(rst, Gpio::kHigh);
+    Gpio::SetPinValue(config_.rst, Gpio::kHigh);
     Systick::Delay_ms(100);
-    Gpio::SetPinValue(rst, Gpio::kLow);
+    Gpio::SetPinValue(config_.rst, Gpio::kLow);
     Systick::Delay_ms(2);
-    Gpio::SetPinValue(rst, Gpio::kHigh);
+    Gpio::SetPinValue(config_.rst, Gpio::kHigh);
     Systick::Delay_ms(100);
-    Gpio::SetPinValue(rst, Gpio::kLow);
+    Gpio::SetPinValue(config_.rst, Gpio::kLow);
     Systick::Delay_ms(100);
-    Gpio::SetPinValue(rst, Gpio::kHigh);
+    Gpio::SetPinValue(config_.rst, Gpio::kHigh);
     Systick::Delay_ms(120000);
 }
 void Tft::DisplayImage(const Array<uint16_t, 20480> image) {
@@ -55,71 +82,57 @@ void Tft::DisplayImage(const Array<uint16_t, 20480> image) {
         SetPixel(image[counter]);
     }
 }
-void Tft::DrawRectangle(uint8_t hight, uint8_t width, uint8_t xaxis, uint8_t yaxis, TftColors color) {  //  NOLINT
-    DrawHLine(xaxis, yaxis, width, color);
-    DrawVLine(xaxis, yaxis, hight, color);
-    DrawHLine(xaxis, hight + yaxis, width, color);
-    DrawVLine(xaxis + width, yaxis, hight, color);
+void Tft::DrawRectangle(uint8_t hight, uint8_t width, uint8_t xAxis, uint8_t yAxis, TftColors color) {  //  NOLINT
+    DrawHLine(xAxis, yAxis, width, color);
+    DrawVLine(xAxis, yAxis, hight, color);
+    DrawHLine(xAxis, yAxis + hight, width, color);
+    DrawVLine(xAxis + width, yAxis, hight, color);
 }
 void Tft::SendCommand(uint8_t command) {
-    Gpio::SetPinValue(A0, Gpio::kLow);
-    TFtSpi.Write(command);
+    Gpio::SetPinValue(config_.A0, Gpio::kLow);
+    config_.TFtSpi.Write(command);
 }
 void Tft::SendData(uint8_t data) {
-    Gpio::SetPinValue(A0, Gpio::kHigh);
-    TFtSpi.Write(data);
+    Gpio::SetPinValue(config_.A0, Gpio::kHigh);
+    config_.TFtSpi.Write(data);
 }
 void Tft::SetAddress(uint16_t startX, uint16_t endX, uint16_t startY, uint16_t endY) {
     if ((startX < 127) && (startY < 159)) {
         //  Set X address
         SendCommand(kCASET);
         //  Start byte
-        SendData(startX >> 8);  //  MS byte
-        SendData(startX);
+        SendData(ExtractBits<uint16_t, 8>(startX));  // MS byte
+        SendData(startX);  // LS byte
         //  Stop byte
-        SendData(endX >> 8);
-        SendData(endX);
+        SendData(ExtractBits<uint16_t, 8>(endX));  // MS byte
+        SendData(endX);  // LS byte
         //  Set Y address
         SendCommand(kRASET);
         //  Start byte
-        SendData(startY >> 8);  //  MS byte
-        SendData(startY);
+        SendData(ExtractBits<uint16_t, 8>(startY));  // MS byte
+        SendData(startY);  // LS byte
         //  Stop byte
-        SendData(endY >> 8);
-        SendData(endY);
+        SendData(ExtractBits<uint16_t, 8>(endY));  // MS byte
+        SendData(endY);  // LS byte
         //  Memory write
         SendCommand(kRAWMR);    //  This tells the display that pixel data will follow.
     }
 }
 void Tft::SetPixel(uint16_t pixel) {
     //  write high byte
-    SendData(pixel >> 8);
+    SendData(ExtractBits<uint16_t, 8>(pixel));
     //  write low byte
     SendData(pixel & 0x00ff);
 }
-void Tft::DrawHLine(uint8_t xaxis, uint8_t yaxis, uint8_t length, TftColors color) {
-    for (; length > 0; length --) {
-        SendCommand(kCASET);
-        SendData(xaxis);
-        SendData(xaxis);
-        SendCommand(kRASET);
-        SendData(yaxis);
-        SendData(yaxis);
-        SendCommand(kRAWMR);
+void Tft::DrawHLine(uint8_t xAxis, uint8_t yAxis, uint8_t length, TftColors color) {
+    SetAddress(xAxis, xAxis + length -1, yAxis, yAxis);
+    for (uint8_t counter = 0; counter < length; counter++) {
         SetPixel(color);
-        xaxis++;
     }
 }
-void Tft::DrawVLine(uint8_t xaxis, uint8_t yaxis, uint8_t length, TftColors color) {
-    for (; length > 0; length --) {
-        SendCommand(kCASET);
-        SendData(xaxis);
-        SendData(xaxis);
-        SendCommand(kRASET);
-        SendData(yaxis);
-        SendData(yaxis);
-        SendCommand(kRAWMR);
+void Tft::DrawVLine(uint8_t xAxis, uint8_t yAxis, uint8_t length, TftColors color) {
+    SetAddress(xAxis, xAxis, yAxis, yAxis + length -1);
+    for (uint8_t counter = 0; counter < length; counter++) {
         SetPixel(color);
-        yaxis++;
     }
 }
