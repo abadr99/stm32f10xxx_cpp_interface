f6bc9fa80a92ebd9973ff23ece2461680ca67972
abadr99 <abdelrhmanezzbadr@outlook.com>
2024-08-31 17:04:44 +0300

    test: fix build issue

--- tests/unittests/mcal/DMA-test.cpp
+++ tests/unittests/mcal/DMA-test.cpp
@@ -12,7 +12,7 @@
 #include "utils/inc/BitManipulation.h"
 #include "mcal/inc/stm32f103xx.h"
 #include "mcal/inc/Pin.h"
-#include "mcal/inc/DMA.h"
+#include "mcal/inc/Dma.h"
 
 using namespace stm32::utils::bit_manipulation;
 using namespace stm32::dev::mcal::pin;
