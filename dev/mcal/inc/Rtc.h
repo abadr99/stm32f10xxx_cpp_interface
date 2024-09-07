1685c085c1e823ce593305a5f9d12305ba1d2cfc
abadr99 <abdelrhmanezzbadr@outlook.com>
2024-09-08 01:27:38 +0300

    fix build issue

--- dev/mcal/inc/Rtc.h
+++ dev/mcal/inc/Rtc.h
@@ -1,6 +1,6 @@
 /**
  * @file RTC.h
- * @author Mohamed Refat
+ * @author
  * @brief 
  * @version 0.1
  * @date 2024-07-26
@@ -27,13 +27,14 @@ struct RtcConfig {
     uint32_t prescaler_value;
     uint32_t counter_value;
     uint32_t alarm_value;
-    pFunction SecondISR;
-    pFunction AlarmISR;
-    pFunction OverflowISR;
+    stm32::type::pFunction SecondISR;
+    stm32::type::pFunction AlarmISR;
+    stm32::type::pFunction OverflowISR;
 };
 
 class Rtc {
  public:
+    using pFunction = stm32::type::pFunction;
     // Initialization and control
     static void Init(const RtcConfig &config);
     static void GetCurrentTime(Time *pCurrent_time);
@@ -49,7 +50,6 @@ class Rtc {
     static void Enable(void);
     static void Disable(void);
     
-
     static pFunction GetAlarmISR(void);
     static pFunction GetOverflowISR(void);
     static pFunction GetSecondISR(void);
