50a6b63074090f518b67f0a72a607fd4c32bba4b
nouragamal <nouragamal@ubuntu.myguest.virtualbox.org>
2024-09-13 19:39:51 +0300

    add Iwdg unit test

--- tests/unittests/mcal/Iwdg-test.cpp
+++ tests/unittests/mcal/Iwdg-test.cpp
@@ -1,9 +1,9 @@
 /**
  * @file Iwdg-test.cpp
- * @author your name (you@domain.com)
+ * @author noura36
  * @brief 
  * @version 0.1
- * @date 2024-07-27
+ * @date 2024-09-13
  * 
  * @copyright Copyright (c) 2024
  * 
@@ -17,3 +17,15 @@ uint32_t IwdgReg[4] = {0x0};
 using namespace stm32::util;
 using namespace stm32::dev::mcal::iwdg;
 using namespace stm32::registers::iwdg;
+
+TEST(IwdgTest, IwdgConstractor) {
+    Iwdg(kDiv4, 1000);
+    EXPECT_EQ(0, IWDG->PR);
+    EXPECT_EQ(1000, IWDG->RLD);
+    EXPECT_EQ(0xCCCC, IWDG->KR);
+}
+TEST(IwdgTest, Refresh) {
+    Iwdg watchdog(kDiv16, 2000);
+    watchdog.Refresh();
+    EXPECT_EQ(0xAAAA, IWDG->KR);
+}
