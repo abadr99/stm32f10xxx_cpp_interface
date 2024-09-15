315b59867166fce9cfffb925c2c52301de7433ff
abadr99 <abdelrhmanezzbadr@outlook.com>
2024-09-16 01:23:55 +0300

    ..

--- tools/github/lcov.sh
+++ tools/github/lcov.sh
@@ -1,6 +1,6 @@
 #/!/bin/sh
 
-lcov --capture --no-external --directory ../../tests/unittests/ --output-file coverage.info
-echo "Displayinh coverage.info"
-cat coverage.info
+lcov --capture --no-external --directory ../../tests/unittests/ --output-file ../../coverage.info
+echo "Displaying coverage.info"
+cat ../../coverage.info
 # genhtml coverage.info --output-directory out
