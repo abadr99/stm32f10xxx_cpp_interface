61a5a16e71d248b3a266b7666938a7fdedfa2f9a
abadr99 <abdelrhmanezzbadr@outlook.com>
2024-09-21 01:46:20 +0300

    `

--- tools/github/windows/configure.ps1
+++ tools/github/windows/configure.ps1
@@ -1,5 +1,5 @@
 # Define download URL and destination
-$downloadUrl = "https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/downloads"
+$downloadUrl = "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a&hash=5631ACEF1F8F237389F14B41566964EC"
 $destinationFolder = "C:\opt"
 $destinationFile = "${destinationFolder}\gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2"
 
