2340d3c6c43facd3b335446b8d3a43188b4cde20
abadr99 <abdelrhmanezzbadr@outlook.com>
2024-09-20 17:34:28 +0300

    fix linter

--- dev/inc/utils/Logger.h
+++ dev/inc/utils/Logger.h
@@ -9,8 +9,8 @@
  * 
  */
 
-#ifndef DEV_UTILS_INC_LOGGER_H_
-#define DEV_UTILS_INC_LOGGER_H_
+#ifndef DEV_INC_UTILS_LOGGER_H_
+#define DEV_INC_UTILS_LOGGER_H_
 
 #include "mcal/Usart.h"
 
@@ -22,19 +22,19 @@ namespace logger {
 
 class Logger {
  public:
-   static void Init(void);
-   static void Info(const char *str);
-   static void Print(const char *str);
-   static void Error(const char *str);
-   static void Warning(const char *str);
+    static void Init(void);
+    static void Info(const char *str);
+    static void Print(const char *str);
+    static void Error(const char *str);
+    static void Warning(const char *str);
 
  private:
-   static void Send(const char *str);
-   static UsartConfig usartConfig_;
-   static Usart usart_;
+    static void Send(const char *str);
+    static UsartConfig usartConfig_;
+    static Usart usart_;
 };
 }  // namespace logger
 }  // namespace utils
 }  // namespace stm32
 
-#endif  // DEV_UTILS_INC_LOGGER_H_
+#endif  // DEV_INC_UTILS_LOGGER_H_
