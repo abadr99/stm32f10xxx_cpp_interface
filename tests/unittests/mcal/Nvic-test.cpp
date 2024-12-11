64d7da86014ad7fac19079b64b1e409436f6a28a
abadr99 <abdelrhmanezzbadr99@outlook.com>
2024-12-12 01:19:08 +0200

    lint: fix linter issues

--- tests/unittests/mcal/Nvic-test.cpp
+++ tests/unittests/mcal/Nvic-test.cpp
@@ -67,4 +67,4 @@ TEST(NvicTest, SetPriorityGroup) {
     Nvic::SetPriorityGroup(kSCB_0GROUP_4SUBGROUP);
     EXPECT_EQ(kSCB_0GROUP_4SUBGROUP,   (ExtractBits<uint32_t, 0, 31>(SCB->AIRCR)));
 }
-#endif 
+#endif
