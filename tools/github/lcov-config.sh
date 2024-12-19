bac6692bcc0090900b9e02619f16a289a8a41ac5
abadr99 <abdelrhmanezzbadr99@outlook.com>
2024-12-19 20:41:12 +0200

    update ci

--- tools/github/lcov-config.sh
+++ tools/github/lcov-config.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 sudo apt-get install lcov
+
--- tools/github/lcov.sh
+++
@@ -1,7 +0,0 @@
-#/!/bin/sh
-pwd
-chmod 777 ../../tests/unittests
-lcov --capture --no-external --directory ../../tests/unittests/ --output-file ../../coverage.info
-echo "Displaying coverage.info"
-cat ../../coverage.info
-# genhtml coverage.info --output-directory out
