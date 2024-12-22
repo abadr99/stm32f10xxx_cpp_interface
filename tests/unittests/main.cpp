d167fc240b895da88e5391c082447f7f164b2d26
abadr99 <abdelrhmanezzbadr99@outlook.com>
2024-12-22 14:48:00 +0200

    stop dealing with gtest as internal lib

--- tests/unittests/main.cpp
+++ tests/unittests/main.cpp
@@ -9,7 +9,7 @@
  * 
  */
 
-#include "C:\Users\runneradmin\vcpkg\installed\x86-windows\include\gtest/gtest.h"
+#include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
