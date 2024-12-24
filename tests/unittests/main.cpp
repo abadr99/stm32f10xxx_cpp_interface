7443a67af070d396c8c1a635c769438fbbedb775
abadr99 <abdelrhmanezzbadr99@outlook.com>
2024-12-25 00:42:31 +0200

    Try to fix

--- tests/unittests/main.cpp
+++ tests/unittests/main.cpp
@@ -12,6 +12,10 @@
 #include <iostream>
 #include "gtest/gtest.h"
 
+TEST(SampleTest, BasicAssertion) {
+    EXPECT_EQ(1 + 1, 2);
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     std::cerr << "[gtest]: Run all tests";
