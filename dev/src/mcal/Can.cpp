aa275781e6064791783b4560ce64191fabc9c95b
noura36 <nouramohamedgamalll@gmail.com>
2025-02-05 21:49:32 +0200

    some changes

--- dev/src/mcal/Can.cpp
+++ dev/src/mcal/Can.cpp
@@ -26,7 +26,15 @@ volatile CANRegDef* Can::CAN = nullptr;
   
 void Can::Init(const CanConfig &conf) {
     CAN = reinterpret_cast<volatile CANRegDef*>(Addr<Peripheral::kCAN>::Get());
+
     //  Exit from sleep mode
+    /*
+    CAN->MCR.INRQ = 1;
+    util::BusyWait<constant::TimeOut::kDefault>([&](){ return (CAN->MSR.INAK); });
+    CAN->MCR.SLEEP = 0; 
+    util::BusyWait<constant::TimeOut::kDefault>([&](){ return !(CAN->MSR.SLAK); });
+    */
+
     SetOperatingMode(conf, OperatingMode::kInitialization);
 
     CAN->MCR.TTCM = static_cast<uint32_t>(conf.TTCM);
@@ -54,6 +62,16 @@ void Can::Init(const CanConfig &conf) {
     CAN->RF1R.RFOM1 = 1;
     //  Request leave initialisation
     SetOperatingMode(conf, OperatingMode::kNormal);
+    /*
+    auto IsTimeOut = [&](){ return !(CAN->MSR.INAK || CAN->MSR.SLAK); };
+    if (conf.opMode == OperatingMode::kSleep) {
+        CAN->MCR.SLEEP = 0;
+        util::BusyWait<constant::TimeOut::kDefault>(IsTimeOut);
+    } else if (conf.opMode == OperatingMode::kInitialization) {
+        CAN->MCR.INRQ = 0;
+        util::BusyWait<constant::TimeOut::kDefault>(IsTimeOut);
+    }
+    */
 }
 void Can::FilterInit(const FilterConfig& conf) {
     CAN->FMR.FINIT = 1;  //  Initialisation mode for the filter
