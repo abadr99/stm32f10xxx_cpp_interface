da6345ac45b740eea607d0eb67e30b4e4bf3aed4
noran97 <noranmahmoud597@gmail.com>
2025-02-10 21:58:54 +0200

    fixing directories

--- dev/inc/hal/LedMatrix.h
+++ dev/inc/hal/LedMatrix.h
@@ -8,35 +8,33 @@
  * @copyright Copyright (c) 2024
  *
  */
-#ifndef DEV_INC_HAL_LEDMATRIX_H_
-#define DEV_INC_HAL_LEDMATRIX_H_
+#ifndef DEV_HAL_INC_LEDMATRIX_H_
+#define DEV_HAL_INC_LEDMATRIX_H_
 
 using namespace stm32::dev::mcal::pin; 
-// using namespace stm32::dev::mcal::gpio;
+using namespace stm32::dev::mcal::gpio;
 
 #include <array>
 
 #include "mcal/Pin.h"
 #include "utils/Font.h"
-#include "utils/Array.h"
 
 using namespace stm32::utils::font;
-using namespace stm32::util;
 
 namespace stm32 {
 namespace dev {
 namespace hal {
 namespace ledMatrix {
 
-enum  MatrixConnectionType : uint8_t {
+enum ConnectionType : uint8_t {
     kCommon_Row_Cathode,
     kCommon_Row_Anode,
 };
 
-template <MatrixConnectionType connectionType>
+template <ConnectionType connectionType>
 class LedMatrix {
  public:
-    using Array_t = util::Array<Pin, 8>;
+    using Array_t = std::array<Pin, 8>;
     LedMatrix(const Array_t rowPins, const Array_t colPins);
     void Init();
     void DrawChar(uint8_t character);
@@ -51,4 +49,4 @@ class LedMatrix {
 }  // namespace hal
 }  // namespace dev
 }  // namespace stm32
-#endif  // DEV_INC_HAL_LEDMATRIX_H_
+#endif  // DEV_HAL_INC_LEDMATRIX_H_
