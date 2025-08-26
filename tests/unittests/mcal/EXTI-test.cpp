248375b711f83707aae5c34e17cb4ba4b6b35541
noura36 <nouramohamedgamalll@gmail.com>
2025-08-26 23:17:51 +0300

    EXTI unittest

--- tests/unittests/mcal/EXTI-test.cpp
+++ tests/unittests/mcal/EXTI-test.cpp
@@ -8,8 +8,8 @@
  * @copyright Copyright (c) 2024
  * 
  */
-#if 0
 #include <gtest/gtest.h>
+#include "utils/Types.h"
 #include "utils/BitManipulation.h"
 #include "mcal/stm32f103xx.h"
 #include "mcal/Pin.h"
@@ -26,46 +26,75 @@ using namespace stm32::registers::gpio;
 uint32_t AFIOReg[7] = {0x0};
 uint32_t EXTIReg[6] = {0x0};
 
-static volatile EXTIRegDef* EXTI;
+class ExtiTest : public testing::Test {
+ protected:
+    void SetUp() override {
+        using Exti_addr = Addr<Peripheral::kEXTI>;
+        using Afio_addr = Addr<Peripheral::kAFIO>;
+        Exti_addr::Set(&EXTIReg[0]);
+        Afio_addr::Set(&AFIOReg[0]);
+        Exti::Init();
+        EXTI = Exti::GetPtr<EXTIRegDef>();
+        AFIO = Exti::GetPtr<AfioRegDef>();
+    }
+
+    template<typename T, typename F>
+    void GenericTest(T* reg, Line line, F extiFunc, uint32_t expectedMask) {
+        *reg = 0;
+        EXTI_Config exti;
+        exti.port = kPortA;
+        exti.line = line;
+        exti.trigger = Trigger::kRising;
+        extiFunc(exti);
+        EXPECT_EQ(expectedMask, *reg);
+    }
+
+    RegWidth_t AFIOReg[7]  = {};
+    RegWidth_t EXTIReg[6]  = {};
+    volatile EXTIRegDef* EXTI;
+    volatile AfioRegDef* AFIO;
+};
 
 
-TEST(EXTItest, Enable) {
+TEST_F(ExtiTest, Enable) {
     EXTI_Config exti;
     exti.port = kPortA;
     exti.line = Line::kExti0;
     exti.trigger = Trigger::kRising;
     Exti::Enable(exti);
-    // EXPECT_EQ(0x0,    AFIO->EXTICRx[0]);
-    EXPECT_EQ(0x1,       EXTI->IMR);
-    EXPECT_EQ(0x1,       EXTI->RTSR);
+
+    EXPECT_EQ(0x1, EXTI->IMR);
+    EXPECT_EQ(0x1, EXTI->RTSR);
 }
 
-TEST(EXTItest, Disable) {
+TEST_F(ExtiTest, Disable) {
     EXTI_Config exti;
-    EXTI->IMR = 0;
     exti.port = kPortB;
     exti.line = Line::kExti10;
     exti.trigger = Trigger::kFalling;
+
     Exti::Disable(exti);
-    EXPECT_EQ(0x0,      EXTI->IMR);  //  EXTI0 is enable in previous test
-    EXPECT_EQ(0x0,      EXTI->FTSR);
+
+    EXPECT_EQ(0x0, EXTI->IMR);
+    EXPECT_EQ(0x0, EXTI->FTSR);
 }
 
-TEST(EXTItest, SetPendingFlag) {
+TEST_F(ExtiTest, SetPendingFlag) {
     EXTI_Config exti;
     exti.port = kPortB;
     exti.line = Line::kExti6;
     exti.trigger = Trigger::kFalling;
+
     Exti::SetPendingFlag(exti);
-    EXPECT_EQ(0x40,       EXTI->PR);
+    EXPECT_EQ(0x40, EXTI->PR);
 }
 
-TEST(EXTItest, DISABLED_ClearPendingFlag) {
+TEST_F(ExtiTest, ClearPendingFlag) {
     EXTI_Config exti;
     exti.port = kPortB;
-    exti.line = Line::kExti16;
+    exti.line = Line::kExti6;
     exti.trigger = Trigger::kFalling;
+
     Exti::ClearPendingFlag(exti);
-    EXPECT_EQ(0x40,       EXTI->PR);
+    EXPECT_EQ(0x0, EXTI->PR);
 }
-#endif
