4d79a490e8f9b8a7f71318d2e95edafeadd0455e
noura36 <nouramohamedgamalll@gmail.com>
2025-09-07 22:20:52 +0300

    DMA unit test

--- tests/unittests/mcal/DMA-test.cpp
+++ tests/unittests/mcal/DMA-test.cpp
@@ -8,7 +8,9 @@
  * @copyright Copyright (c) 2024
  * 
  */
+#if 0
 #include <gtest/gtest.h>
+#include "utils/Types.h"
 #include "utils/BitManipulation.h"
 #include "mcal/stm32f103xx.h"
 #include "mcal/Pin.h"
@@ -20,4 +22,47 @@ using namespace stm32::dev::mcal::dma;
 using namespace stm32::registers::dma;
 using namespace stm32::registers::gpio;
 
-uint32_t DmaReg[30] = {0x0};
+class DmaTest : public testing::Test {
+ protected:
+    void SetUp() override {
+        using Dma_addr = Addr<Peripheral::kDMA1>;
+        Dma_addr::Set(&DmaReg[0]);
+        Dma::Init(config);
+        DMA = Dma::GetPtr();
+    }
+
+    uint32_t DmaReg[30] = {};
+    DMAConfig config = {
+        .channel = kChannel1,
+        .dir = kMem2Mem,
+        .mode = kNoCircular,
+        .sourceBaseAddr = 0x20000000,
+        .destinationBaseAddr = 0x40020000,
+        .bufferSize = 128,
+        .peripheralIncrementState = kEnable,
+        .memIncrementState = kEnable,
+        .peripheralDataSize = k32bit,
+        .memoryDataSize = k32bit,
+        .channelPriority = kHigh,
+        .transErrorInterrupt = kEnable,
+        .transCompleteInterrupt = kEnable,
+    };
+    volatile DMARegDef* DMA;
+};
+TEST_F(DmaTest, EnableDisableChannel) {
+    Dma::Enable(config.channel);
+    EXPECT_EQ(1, (ExtractBits<uint32_t, 0> (DMA->CHANNEL[config.channel].CCR.registerVal)));
+    Dma::Disable(config.channel);
+    EXPECT_EQ(0, (ExtractBits<uint32_t, 0> (DMA->CHANNEL[config.channel].CCR.registerVal)));
+}
+TEST_F(DmaTest, SetPointerToTransferCompleteISR) {
+    auto dummyISR = [](){};
+    Dma::SetPointerToTransferCompleteISR(config.channel, dummyISR);
+    EXPECT_EQ(dummyISR, Dma::GetPointerToTransferCompleteISR(config.channel));
+}
+TEST_F(DmaTest, SetPointerToTransferErrorISR) {
+    auto dummyISR = [](){};
+    Dma::SetPointerToTransferErrorISR(config.channel, dummyISR);
+    EXPECT_EQ(dummyISR, Dma::GetPointerToTransferErrorISR(config.channel));
+}
+#endif
